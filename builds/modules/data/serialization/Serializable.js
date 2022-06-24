d39d16aa6e4402d391a9e370b9aebb3e31bbab19
Mikel Salazar <contact@mikelsalazar.com>
2022-06-24 09:44:30 +0200

    Improved data structures

--- builds/modules/data/serialization/Serializable.js
+++
@@ -1,9 +0,0 @@
-import { Item } from "../Item.js";
-import { Type } from "../Type.js";
-
-/** Defines an object ready for serialization */
-export class Serializable extends Item {
-}
-
-/** The metadata of the data type. */
-Serializable.type = new Type(Serializable, Item.type);
